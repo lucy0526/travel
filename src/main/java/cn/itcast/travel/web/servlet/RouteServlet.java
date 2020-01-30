82ed132a697a3163646f2be79e0d61829e9f988c
lucy0526 <906404159@qq.com>
2020-01-30 09:39:00 +0800

    5' ;

--- src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
@@ -1,5 +1,10 @@
 package cn.itcast.travel.web.servlet;
 
+import cn.itcast.travel.domain.PageBean;
+import cn.itcast.travel.domain.Route;
+import cn.itcast.travel.service.RouteService;
+import cn.itcast.travel.service.impl.RouteServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +13,46 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/route/*")
-public class RouteServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class RouteServlet extends BaseServlet {
+    private RouteService routeService = new RouteServiceImpl();
+
+    /**
+     * 分页处理
+     */
+    public void pageQuery(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String currentPageStr = request.getParameter("currentPage");
+        String pageSizeStr = request.getParameter("pageSize");
+        String cidStr = request.getParameter("cid");
+        String rname = request.getParameter("rname");
+        rname = new String(rname.getBytes("iso-8859-1"), "utf-8");
+
+        int cid = 0;
+//        将字符串转为数字
+        if (!"null".equalsIgnoreCase(cidStr) && cidStr != null && cidStr.length() > 0){
+            cid = Integer.parseInt(cidStr);
+        }
+
+        int currentPage = 1;
+        if (currentPageStr != null && currentPageStr.length() > 0) {
+            currentPage = Integer.parseInt(currentPageStr);
+        }
+
+        int pageSize = 5;
+        if (pageSizeStr != null && pageSizeStr.length() > 0) {
+            pageSize = Integer.parseInt(pageSizeStr);
+        }
+
+        //查询数据库
+        PageBean<Route> pb = routeService.pageQuery(cid, currentPage, pageSize, rname);
+        writeValue(response, pb);
     }
 
+    /**
+     * 详细信息
+     */
+    public void findOne(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String rid = request.getParameter("rid");
+        Route route = routeService.findOne(rid);
+        writeValue(response, route);
+    }
 }
